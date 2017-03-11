49270d4a44b4cbccddb524a3c3ab914a2c2f1374
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-11 21:25:00 +0800

    add index action and fetch all items and show on item index

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -15,3 +15,10 @@ Tally.Repo.delete_all Tally.User
 
 Tally.User.changeset(%Tally.User{}, %{name: "Test User", email: "testuser@example.com", password: "secret", password_confirmation: "secret"})
 |> Tally.Repo.insert!
+
+
+
+# Tally.Repo.delete_all Tally.Item
+#
+# Tally.Item.changeset(%Tally.Item{}, %{name: "Lenovo Laptop", serial: "X657231", used_by: "Jetro Roa", quantity: "1"})
+# |> Tally.Repo.insert!
--- web/controllers/item_controller.ex
+++ web/controllers/item_controller.ex
@@ -1,10 +1,11 @@
 defmodule Tally.ItemController do
   use Tally.Web, :controller
 
-
+  alias Tally.Item
 
   def index(conn, params) do
-    render conn, "index.html"
+    items = Repo.all(Item)
+    render conn, "index.html", items: items
   end
 
 end
