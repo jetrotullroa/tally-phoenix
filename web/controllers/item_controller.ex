ab6db2e4e3636dc32067067a82d4e221d4553773
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-11 21:39:53 +0800

    set initial actions on item controller

--- web/controllers/item_controller.ex
+++ web/controllers/item_controller.ex
@@ -8,4 +8,28 @@ defmodule Tally.ItemController do
     render conn, "index.html", items: items
   end
 
+  def show(conn, params) do
+     
+  end
+
+  def new(conn, params) do
+
+  end
+
+  def create(conn, params) do
+
+  end
+
+  def edit(conn, params) do
+
+  end
+
+  def update(conn, params) do
+
+  end
+
+  def delete(conn, params) do
+
+  end
+
 end
